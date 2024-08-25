62aabc3c2798f12a99269198c723d8cf11dd0da9
Viktor Plane <viktor@planevin.se>
2024-08-25 09:45:49 +0200

    feat: first commit

---
+++ src/utils/serialization.ts
@@ -0,0 +1,20 @@
+import { TokenMetadata } from "../types";
+
+export function serializeTokenMetadata(metadata: TokenMetadata): any[] {
+  const yours: any[] = [
+    metadata.yours.modules,
+    [metadata.yours.project.name, metadata.yours.project.owner_id],
+    metadata.yours.collection,
+  ];
+
+  const result: any[] = [
+    metadata.name,
+    JSON.stringify(metadata.properties),
+    yours,
+    metadata.description ?? null,
+    metadata.image ?? null,
+    metadata.animation_url ?? null,
+  ];
+
+  return result;
+}
