8fa00d7422baaf70b746218a6f2e28a5dc9c0c7d
Viktor Plane <viktor@planevin.se>
2024-12-03 19:54:20 +0100

    fix: pass target chain in crosschain

--- src/core/utils/crosschain.ts
+++ src/core/utils/crosschain.ts
@@ -19,68 +19,78 @@ export async function performCrossChainTransfer(
     fromSession
       .transactionBuilder()
       .add(nop())
-      .add(op('yours.init_transfer', toAccountId, tokenId, amount, metadata), {
-        onAnchoredHandler: (initData: any) => {
-          if (!initData) {
-            reject(new Error('No data provided after init_transfer'));
-            return;
-          }
+      .add(
+        op(
+          'yours.init_transfer',
+          Buffer.from(toChain.config.blockchainRid, 'hex'),
+          toAccountId,
+          tokenId,
+          amount,
+          metadata
+        ),
+        {
+          onAnchoredHandler: (initData: any) => {
+            if (!initData) {
+              reject(new Error('No data provided after init_transfer'));
+              return;
+            }
 
-          initData
-            .createProof(toChain.config.blockchainRid)
-            .then((iccfProofOperation: any) => {
-              return transactionBuilder(noopAuthenticator, toChain)
-                .add(iccfProofOperation, {
-                  authenticator: noopAuthenticator,
-                })
-                .add(
-                  op('yours.apply_transfer', initData.tx, initData.opIndex),
-                  {
+            initData
+              .createProof(toChain.config.blockchainRid)
+              .then((iccfProofOperation: any) => {
+                return transactionBuilder(noopAuthenticator, toChain)
+                  .add(iccfProofOperation, {
                     authenticator: noopAuthenticator,
-                    onAnchoredHandler: (applyData: any) => {
-                      if (!applyData) {
-                        reject(
-                          new Error('No data provided after apply_transfer')
-                        );
-                        return;
-                      }
+                  })
+                  .add(
+                    op('yours.apply_transfer', initData.tx, initData.opIndex),
+                    {
+                      authenticator: noopAuthenticator,
+                      onAnchoredHandler: (applyData: any) => {
+                        if (!applyData) {
+                          reject(
+                            new Error('No data provided after apply_transfer')
+                          );
+                          return;
+                        }
 
-                      applyData
-                        .createProof(fromSession.blockchainRid)
-                        .then((iccfProofOperation: any) => {
-                          return fromSession
-                            .transactionBuilder()
-                            .add(iccfProofOperation, {
-                              authenticator: noopAuthenticator,
-                            })
-                            .add(
-                              op(
-                                'yours.complete_transfer',
-                                applyData.tx,
-                                applyData.opIndex
-                              ),
-                              {
+                        applyData
+                          .createProof(fromSession.blockchainRid)
+                          .then((iccfProofOperation: any) => {
+                            return fromSession
+                              .transactionBuilder()
+                              .add(iccfProofOperation, {
                                 authenticator: noopAuthenticator,
-                              }
-                            )
-                            .buildAndSend();
-                        })
-                        .then(() => {
-                          resolve();
-                        })
-                        .catch((error: Error) => {
-                          reject(error);
-                        });
-                    },
-                  }
-                )
-                .buildAndSendWithAnchoring();
-            })
-            .catch((error: Error) => {
-              reject(error);
-            });
-        },
-      })
+                              })
+                              .add(
+                                op(
+                                  'yours.complete_transfer',
+                                  applyData.tx,
+                                  applyData.opIndex
+                                ),
+                                {
+                                  authenticator: noopAuthenticator,
+                                }
+                              )
+                              .buildAndSend();
+                          })
+                          .then(() => {
+                            resolve();
+                          })
+                          .catch((error: Error) => {
+                            reject(error);
+                          });
+                      },
+                    }
+                  )
+                  .buildAndSendWithAnchoring();
+              })
+              .catch((error: Error) => {
+                reject(error);
+              });
+          },
+        }
+      )
       .buildAndSendWithAnchoring()
       .catch((error) => {
         reject(error);
